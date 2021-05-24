d5f3782cc392bcb8676ad1a0753ae0d22c572548
avri2 <avri223@walla.com>
2021-05-24 16:58:02 +0300

    serverContact.js set data and get data functions

--- Client/public/serverContact.js
+++ Client/public/serverContact.js
@@ -1,40 +1,18 @@
-const data = {
-    speed: [1, 2, 3, 4],
-    deg: [2, 4, 6, 8]
-};
-const data1 = {
-    speed: [1, 2, 3, 4],
-    deg: [10, 31, 72, 641]
-};
-
-const optionPost = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-};
-const optionPostDetect = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data1)
-};
-
-const optionDelete = {
-    method: 'DELETE'
-};
-
-let type = 'hybrid';
+let data;
 let id;
+let models;
 
 addNewModel = function (type) {
+    const optionPost = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: data
+    };
     fetch(`/api/model?model_type=${type}`, optionPost).then(res => res.json()).then(model => id = model.model_id);
-
-
 }
-getDetectorByID = function (id) {
+getModelById = function (id) {
     setTimeout(() => fetch(`/api/model?model_id=${id}`).then(res => {
             if (res.ok) {
                 res.json().then(console.log);
@@ -45,24 +23,89 @@ getDetectorByID = function (id) {
 
 }
 deleteModelByID = function (id) {
-    fetch(`/api/model?model_id=${id}`, optionDelete).then(r => {
+    const optionDelete = {
+        method: 'DELETE'
+    };
+    fetch(`/api/model?model_id=${id}`, optionDelete).then(() => {
         console.log("deleted");
     });
 }
 getModels = function () {
-    let models;
     fetch('/api/models').then((result) => {
         models = result.json();
-        return models;
-    }).then(printRes);
+    }).then(notifyList);
+};
+notifyList = function (result) {
+
+};
+
+getData = function () {
+    document.getElementById("demo").innerHTML = data.deg;
+};
+getCurrentData = function () {
+    return data;
 }
-printRes = function (result) {
-    for (let i = 0; i < result.models.length; i++) {
-        console.log(result.models[i]);
-    }
+
+setData = function (fileInput) {
+    if (fileInput.files.length == 0)
+        return;
+    if (fileInput.files[0].type != "application/vnd.ms-excel")
+        return;
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+        let array = event.target.result.split('\r');
+        let result = [];
+        let headers = array[0].split(",");
+        let obj = {};
+        for (let i = 0; i < headers.length; i++) {
+            obj[headers[i]] = [];
+        }
+        for (let i = 1; i < array.length - 1; i++) {
+            let str = array[i];
+            let s = '';
+            let flag = 0;
+            for (let ch of str) {
+                if (ch === '"' && flag === 0) {
+                    flag = 1
+                } else if (ch === '"' && flag == 1) flag = 0;
+                if (ch === ',' && flag === 0) ch = '|';
+                if (ch !== '"') s += ch;
+            }
+            let properties = s.split("|");
+            for (let j in headers) {
+                if (properties[j].includes(",")) {
+                    let string = properties[j].split(",").map(item => item.trim());
+                    obj[headers[j]].push(parseFloat(string[0]));
+                } else obj[headers[j]].push(parseFloat(properties[j]));
+            }
+        }
+        data = JSON.stringify(obj);
+        notifyDataSet();
+    });
+    reader.readAsText(fileInput.files[0]);
+}
+
+notifyDataSet = function () {
+
+}
+
+onDrag = function (evt) {
+    evt.preventDefault();
+}
+
+onDrop = function (evt, fileInput) {
+    fileInput.files = evt.dataTransfer.files;
+    evt.preventDefault();
 }
 
 detect = function (id) {
+    const optionPostDetect = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: data
+    };
     setTimeout(() => fetch(`/api/anomaly?model_id=${id}`, optionPostDetect).then(res => {
             if (res.ok) {
                 res.json().then(console.log);
@@ -71,3 +114,5 @@ detect = function (id) {
         }),
         5000);
 }
+
+getModels();
