b873bfc71869dd047c151c717717295d44c09274
Adam Shapira <50743751+afekaoh@users.noreply.github.com>
2021-05-29 17:32:54 +0300

    reorganize code

--- Client/public/ViewsJS/DropBox.js
+++ Client/public/ViewsJS/DropBox.js
@@ -1,32 +1,3 @@
-let parser = function (result) {
-    let array = result.split('\r');
-    let headers = array[0].split(',');
-    let data = {};
-    for (let i = 0; i < headers.length; i++) {
-        data[headers[i]] = [];
-    }
-    for (let i = 1; i < array.length - 1; i++) {
-        let str = array[i];
-        let s = '';
-        let flag = 0;
-        for (let ch of str) {
-            if (ch === '"' && flag === 0) {
-                flag = 1;
-            } else if (ch === '"' && flag === 1) flag = 0;
-            if (ch === ',' && flag === 0) ch = '|';
-            if (ch !== '"') s += ch;
-        }
-        let properties = s.split('|');
-        for (let j in headers) {
-            if (properties[j].includes(',')) {
-                let string = properties[j].split(',').map(item => item.trim());
-                data[headers[j]].push(parseFloat(string[0]));
-            } else data[headers[j]].push(parseFloat(properties[j]));
-        }
-    }
-    return data;
-};
-
 
 onDragEnter = function (evt) {
     evt.preventDefault();
@@ -67,9 +38,4 @@ updateDrop = function (evt, fileInput) {
         let dr = document.getElementById('dropContainer');
         dr.innerText = 'Wrong amount of files. Drop one CSV file!';
     }
-};
-
-CSV_reader = async function (fileInput) {
-    if (!fileInput || fileInput.files.length === 0 || fileInput.files[0].type !== 'application/vnd.ms-excel') return;
-    return fileInput.files[0].text().then(parser);
 };
